abe914fcf4a9ce522a30dac452a357b5cdb24c95
Adnan Dakhani <dakhani.an01@gmail.com>
2023-06-18 10:47:48 +0530

    second commit

---
+++ TypeConversion.js
@@ -0,0 +1,16 @@
+/*let age = prompt("How Old Are You?");
+age = Number(age);
+age += 1;
+
+console.log("You will be", age, "years old next year");*/
+
+let x;
+let y;
+let z;
+x = Number("14");
+y = String(14);
+z = Boolean(14);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
